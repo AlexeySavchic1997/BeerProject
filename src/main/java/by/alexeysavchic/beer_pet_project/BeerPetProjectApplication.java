d8902bf4a9d9eb5c5e070ade2f25d6c375547abf
AlexeySavchic1997 <aliaksei.sauchyts@innowise.com>
2025-12-05 11:56:48 +0300

    start commit

---
+++ src/main/java/by/alexeysavchic/beer_pet_project/BeerPetProjectApplication.java
@@ -0,0 +1,13 @@
+package by.alexeysavchic.beer_pet_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerPetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/alexeysavchic/beer_pet_project/BeerPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.alexeysavchic.beer_pet_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
